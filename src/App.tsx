13d939d1c81835eadfc561d8c1d40c458dbb8066
Alina <alina.hasan.aman@gmail.com>
2024-11-18 14:56:10 +0600

    update buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import './App.css';
 export default function Square() {
   return (
     <>
-      <button className="button-base button-no-right-border">X</button>
-      <button className="button-base button-no-right-border">X</button>
-      <button className="button-base button-no-right-border">X</button>
-      <button className="button-base button-no-right-border">X</button>
-      <button className="button-base button-no-right-border">X</button>
-      <button className="button-base button-no-right-border">X</button>
-      <button className="button-base button-no-right-border">X</button>
-      <button className="button-base button-no-right-border">X</button>
-      <button className="button-base">X</button> {/* Last button without right border */}
+      <div className='board-row'>
+        <button className="button-base  no-border-r">1</button>
+        <button className="button-base  no-border-r">2</button>
+        <button className="button-base">3</button>
+      </div>
+      <div className='board-row'>
+        <button className="button-base no-border-r no-border-t">4</button>
+        <button className="button-base no-border-r no-border-t">5</button>
+        <button className="button-base no-border-t">6</button>
+      </div>
+      <div className='board-row'>
+        <button className="button-base no-border-r no-border-t">7</button>
+        <button className="button-base no-border-r no-border-t">8</button>
+        <button className="button-base no-border-t">9</button>
+      </div>
     </>
   );
 }
