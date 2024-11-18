ed69569882df84b093eee874ea322d7a7738b4e5
Alina <alina.hasan.aman@gmail.com>
2024-11-18 14:50:21 +0600

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function Square() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button className="button-base button-no-right-border">X</button>
+      <button className="button-base button-no-right-border">X</button>
+      <button className="button-base button-no-right-border">X</button>
+      <button className="button-base button-no-right-border">X</button>
+      <button className="button-base button-no-right-border">X</button>
+      <button className="button-base button-no-right-border">X</button>
+      <button className="button-base button-no-right-border">X</button>
+      <button className="button-base button-no-right-border">X</button>
+      <button className="button-base">X</button> {/* Last button without right border */}
+    </>
   );
 }
-
-export default App;
